306d6bdfc88e4d2d627c2b4aacbd6b8db2a0512b
najm09 <fnajm09@gmail.com>
2020-01-05 15:32:29 +0530

    ammend

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-var button = document.getElementById('change-background');
+var button = document.getElementById("change-background");
 
-button.addEventListener( 'click' , function () {
-	document.body.style.backgroundcolor = 'tomato';
+button.addEventListener( "click" , function () {
+	document.body.style.backgroundcolor = "tomato";
 });
 
-var button1 = document.getElementById('change-background1');
+var button1 = document.getElementById("change-background1");
 
-var button2 = document.getElementById('change-background2');
+var button2 = document.getElementById("change-background2");
 
-var button3 = document.getElementById('change-background3');
+var button3 = document.getElementById("change-background3");
 
-button1.addEventListener( 'dblclick' , function () {
-	document.body.style.backgroundcolor = 'orange';
+button1.addEventListener( "dblclick" , function () {
+	document.body.style.backgroundcolor = "orange";
 });
-button2.addEventListener( 'mouseenter' , function () {
-	document.body.style.backgroundcolor = 'blue';
+button2.addEventListener( "mouseenter", function () {
+	document.body.style.backgroundcolor = "blue";
 });
-button3.addEventListener( 'mouseout' , function () {
-	document.body.style.backgroundcolor = 'yellow';
+button3.addEventListener( "mouseout" , function () {
+	document.body.style.backgroundcolor = "yellow";
 });
