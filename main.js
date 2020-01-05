4c107070dd2e3288b5e6c4af9661fae149c2b46f
najm09 <fnajm09@gmail.com>
2020-01-05 17:58:09 +0530

    ammend

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 var button = document.getElementById('change-background');
 
-button.addEventListener('click',function(){
+button.addElementListener('click',function () {
 	document.body.style.backgroundcolor = 'tomato';
 });
 
 var button1 = document.getElementById('change-background1');
 
-button1.addEventListener('dblclick',function(){
-	document.body.style.backgroundcolor = 'blue';
+button1.addElementListener('dblclick',function(){
+	document.body.style.backgroundcolor = 'orange';
 });
 
 var button2 = document.getElementById('change-background2');
 
-button2.addEventListener('mouseenter',function(){
-	document.body.style.backgroundcolor = 'orange';
+button2.addElementListener('mouseenter',function(){
+	document.body.style.backgroundcolor = 'blue';
 });
 
 var button3 = document.getElementById('change-background3');
 
-button3.addEventListener('mouseout',function(){
+button3.addElementListener('mouseenter',function(){
 	document.body.style.backgroundcolor = 'yellow';
-} );
+});
