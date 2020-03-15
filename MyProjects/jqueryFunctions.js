7d8cc0f5862a666b53d2ca78bbbb3ca728d7b51d
najm09 <fnajm09@gmail.com>
2020-03-15 15:18:15 +0530

    basics of jQuery functions

--- MyProjects/jqueryFunctions.js
+++ MyProjects/jqueryFunctions.js
@@ -3,3 +3,36 @@ $(document).ready(function(){
     $("#head").hide();
   });
 });
+$(document).ready(function(){
+  $("#head").on ({
+    mouseenter : function(){
+      $(this).css("backgroundColor", "yellow");
+    },
+    mouseleave : function(){
+      $(this).css("backgroundColor", "pink");
+    },
+    click : function(){
+      $(this).css("backgroundColor", "cyan");
+    }
+  });
+});
+
+$(document).ready(function(){
+  $("#visible").click(function(){
+    $("#head").show();
+  });
+});
+
+$(document).ready(function(){
+  $("#toggle").click(function(){
+    $("#line").toggle(1000);
+  });
+});
+
+$(document).ready(function(){
+  $("#fade").click(function(){
+    $("#first").fadeToggle();
+    $("#second").fadeToggle(2000);
+    $("#third").fadeToggle(3000);
+  });
+});
