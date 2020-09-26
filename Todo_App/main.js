e44c9b6f5f1c3ca2dbdc31beceeccfe090318171
najm09 <fnajm09@gmail.com>
2020-09-26 23:29:20 +0530

    ui fixes

--- Todo_App/main.js
+++ Todo_App/main.js
@@ -1,62 +1,63 @@
+var form = document.querySelector('form');
+var todolist = document.querySelector('ul');
+var button = document.getElementById('clear');
+var input = document.getElementById('user-todo');
 
-  var form = document.querySelector('form');
-  var todolist = document.querySelector('ul');
-  var button = document.querySelector('button');
-  var input = document.getElementById('user-todo');
+// declare variable 'todosArray' to our todos.
+// use localStorage setItem method to set todos to a string with JSON.stringify().
+// JSON.stringify() is used because localStorage works with strings.this is how we 
+// sent data to local storage.
 
-  // declare variable 'todosArray' to our todos.
-  // use localStorage setItem method to set todos to a string with JSON.stringify().
-  // JSON.stringify() is used because localStorage works with strings.this is how we 
-  // sent data to local storage.
+//we want to ask if there are already 'todos' in localStorage. if so, then we will get those 'todos'
+//using JSON.parse . if localStorage doesnot have any 'todos' then we will set our 'todosArray' to
+//an empty array. 
 
-  //we want to ask if there are already 'todos' in localStorage. if so, then we will get those 'todos'
-  //using JSON.parse . if localStorage doesnot have any 'todos' then we will set our 'todosArray' to
-  //an empty array. 
-
-  var todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
-
+var todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
 
 
-  localStorage.setItem('todos',JSON.stringify(todosArray));
 
-  // Declare a variable storage that contains all the information in local storage .
-  // we will assign to storage JSON.parse() method that converts string from local 
-  // storage into data we can access with Javascript.
-  //when recieving the data from a web server the data is always a string.
-  //parse the data with JSON.parse(),and the data becomes a javascript object.
+localStorage.setItem('todos', JSON.stringify(todosArray));
 
-  var storage = JSON.parse(localStorage.getItem('todos'));
+// Declare a variable storage that contains all the information in local storage .
+// we will assign to storage JSON.parse() method that converts string from local 
+// storage into data we can access with Javascript.
+//when recieving the data from a web server the data is always a string.
+//parse the data with JSON.parse(),and the data becomes a javascript object.
+
+var storage = JSON.parse(localStorage.getItem('todos'));
 
 
-  form.addEventListener('submit',function(e){
-      e.preventDefault();
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-      //push onto 'todosArray' the 'input.value'
+    //push onto 'todosArray' the 'input.value'
 
-      todosArray.push(input.value);
-      
-      //on the localStorage now use 'setItem()' for the key 'todos' the value
-      // of the todosArray as a string with the 'JSON.stringify()' method.
-  
-      localStorage.setItem('todos',JSON.stringify(todosArray));
-      todoMaker(input.value);
-      input.value = "";
-  });
+    todosArray.push(input.value);
 
-  var todoMaker = function(text){
-      var todo = document.createElement('li');
-      todo.textContent = text;
-      todolist.appendChild(todo);
-  }
+    //on the localStorage now use 'setItem()' for the key 'todos' the value
+    // of the todosArray as a string with the 'JSON.stringify()' method.
 
-  for (var i = 0; i < storage.length; i++) {
-todoMaker(storage[i]);
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    todoMaker(input.value);
+    input.value = "";
+});
+
+var todoMaker = function (text) {
+    document.getElementById("todo-header").innerHTML="Todos<hr/>";
+    var todo = document.createElement('li');
+    todo.textContent = text;
+    todolist.appendChild(todo);
+}
+
+for (var i = 0; i < storage.length; i++) {
+    todoMaker(storage[i]);
 }
 
 
-  button.addEventListener('click',function(){
-      localStorage.clear();
-      while(todolist.firstChild){
-          todolist.removeChild(todolist.firstChild);
-      }
-  });
+button.addEventListener('click', function () {
+    localStorage.clear();
+    while (todolist.firstChild) {
+        todolist.removeChild(todolist.firstChild);
+    }
+    document.getElementById("todo-header").innerHTML = " ";
+});
--- memoryGame/main.js
+++ memoryGame/main.js
@@ -1,98 +1,99 @@
-  var cardsArray = [
-    {    'name': 'CSS',    'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true',  },
-    {    'name': 'HTML',    'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true',  },
-    {    'name': 'jQuery',    'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true',  },
-    {    'name': 'JS',    'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true',  },
-    {    'name': 'Node',    'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true',  },
-    {    'name': 'Photo Shop',    'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true',  },
-    {    'name': 'PHP',    'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true',  },
-    {    'name': 'Python',    'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true',  },
-    {    'name': 'Ruby',    'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true',  },
-    {    'name': 'Sass',    'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true',  },
-    {    'name': 'Sublime',    'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true',  },
-    {    'name': 'Wordpress',    'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true',  },
-  ];
-  var gameGrid = cardsArray.concat(cardsArray);
-  gameGrid.sort(function(){
-      return 0.5-Math.random();
-  });
-  var Game = document.getElementById('game-board');
-  var Grid = document.createElement('section');
-  Grid.setAttribute('class','grid');
-  Game.appendChild(Grid);
+var cardsArray = [
+    { 'name': 'CSS', 'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true', },
+    { 'name': 'HTML', 'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true', },
+    { 'name': 'jQuery', 'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true', },
+    { 'name': 'JS', 'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true', },
+    { 'name': 'Node', 'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true', },
+    { 'name': 'Photo Shop', 'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true', },
+    { 'name': 'PHP', 'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true', },
+    { 'name': 'Python', 'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true', },
+    { 'name': 'Ruby', 'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true', },
+    { 'name': 'Sass', 'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true', },
+    { 'name': 'Sublime', 'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true', },
+    { 'name': 'Wordpress', 'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true', },
+];
+var gameGrid = cardsArray.concat(cardsArray);
+gameGrid.sort(function () {
+    return 0.5 - Math.random();
+});
+var Game = document.getElementById('game-board');
+var Grid = document.createElement('section');
+Grid.setAttribute('class', 'grid');
+Game.appendChild(Grid);
 
-  var count = 0;
-  var previousTarget = null;
-  var firstGuess = '';
-  var secondGuess = '';
-  var delay = 120;
+var count = 0;
+var previousTarget = null;
+var firstGuess = '';
+var secondGuess = '';
+var delay = 120;
 
-  var resetGuesses = function(){
-      count = 0;
-      previousTarget = null;
-      firstGuess = '';
-      secondGuess = '';
+var resetGuesses = function () {
+    count = 0;
+    previousTarget = null;
+    firstGuess = '';
+    secondGuess = '';
 
-      var selected = document.querySelectorAll('.selected');
+    var selected = document.querySelectorAll('.selected');
 
-      for(i=0 ; i<selected.length ; i++){
-          selected[i].classList.remove('selected');
-      }
-  }
+    for (i = 0; i < selected.length; i++) {
+        selected[i].classList.remove('selected');
+    }
+}
 
-  var match = function(){
-      var selected = document.querySelectorAll('.selected');
-      for(i=0 ; i<selected.length ; i++){
-          selected[i].classList.add('match');
-      }
-  };
+var match = function () {
+    var selected = document.querySelectorAll('.selected');
+    for (i = 0; i < selected.length; i++) {
+        selected[i].classList.add('match');
+    }
+};
 
-  for (i=0 ; i<gameGrid.length ; i++){
-      // create a div element and assign to variable card
-  var card = document.createElement('div');
-  // Apply a card class to that div
-  card.classList.add('card');
-  // Set the data-name attribute of the div to the cardsArray name
-  card.dataset.name = gameGrid[i].name;
+for (i = 0; i < gameGrid.length; i++) {
+    // create a div element and assign to variable card
+    var card = document.createElement('div');
+    // Apply a card class to that div
+    card.classList.add('card');
+    // Set the data-name attribute of the div to the cardsArray name
+    card.dataset.name = gameGrid[i].name;
 
-  // Create front of card
-  var front = document.createElement('div');
-  front.classList.add('front');
+    // Create front of card
+    var front = document.createElement('div');
+    front.classList.add('front');
 
-  // Create back of card
-  var back = document.createElement('div');
-  back.classList.add('back');
-  back.style.backgroundImage = `url(${gameGrid[i].img})`;
+    // Create back of card
+    var back = document.createElement('div');
+    back.classList.add('back');
+    back.style.backgroundImage = `url(${gameGrid[i].img})`;
 
-  // Append card to grid
-  Grid.appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
-  }
+    // Append card to grid
+    Grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+}
 
-  Grid.addEventListener('click',function(event){
-      var clicked = event.target;
+Grid.addEventListener('click', function (event) {
+    var clicked = event.target;
 
-      if(clicked.nodeName == 'SECTION' || clicked == previousTarget || clicked.parentNode.classList.contains('match') || clicked.parentNode.classList.contains('selected')){
-          return;
-      }
-      if(count < 2){
-          count++;
-          if(count == 1){
-              firstGuess = clicked.parentNode.dataset.name;
-              clicked.parentNode.classList.add('selected');
-          }else{
-              secondGuess = clicked.parentNode.dataset.name;
-              clicked.parentNode.classList.add('selected');
-          }
-          if(firstGuess !== '' && secondGuess !== ''){
-              if(firstGuess==secondGuess){
-                  setTimeout (match,delay);
-                  setTimeout (resetGuesses,delay);
-              }else{
-                setTimeout (resetGuesses,delay);
-              }
-          }
-      }
-      previousTarget = clicked;
-  });
+    if (clicked.nodeName == 'SECTION' || clicked == previousTarget || 
+    clicked.parentNode.classList.contains('match') || clicked.parentNode.classList.contains('selected')){
+        return;
+    }
+    if (count < 2) {
+        count++;
+        if (count == 1) {
+            firstGuess = clicked.parentNode.dataset.name;
+            clicked.parentNode.classList.add('selected');
+        } else {
+            secondGuess = clicked.parentNode.dataset.name;
+            clicked.parentNode.classList.add('selected');
+        }
+        if (firstGuess !== '' && secondGuess !== '') {
+            if (firstGuess == secondGuess) {
+                setTimeout(match, delay);
+                setTimeout(resetGuesses, delay);
+            } else {
+                setTimeout(resetGuesses, delay);
+            }
+        }
+    }
+    previousTarget = clicked;
+});
